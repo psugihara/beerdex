e9ee76d6ca424874c5f8664bbd36dfc3c8f015b9
Peter Sugihara <peter@campsh.com>
2012-12-10 10:57:29 -0500

    commented get_pics.py

--- data/get_pics.py
+++ data/get_pics.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
+###############################################################################
+# get_pics.py
+#
+# This program fetches images that match a query string provided on the
+# command line.
+#
+# Created by Peter Sugihara
+###############################################################################
+
 import sys
 from os import mkdir, chdir, path
 from apiclient.discovery import build
 import requests
 
-NUM = 10
+PER_PAGE = 10
 
+# Download images to the current directory given an array of URL's.
 def download_images(links):
     for i, l in enumerate(links):
         try:
@@ -20,7 +30,10 @@ def download_images(links):
 
 if __name__ == '__main__':
 
+    # Join all arguments to form the query string.
     query = ' '.join(sys.argv[1:])
+
+    # Name the directory after the query string.
     directory = '_'.join(sys.argv[1:])
 
     print 'query: "' + query + '"'
@@ -29,6 +42,7 @@ if __name__ == '__main__':
     service = build("customsearch", "v1",
             developerKey="AIzaSyDx23eWflm8I0An6JZh8wN25_nDSNlobKs")
 
+    # Fetch image links from Google a page at a time.
     links = []
     for page in xrange(1,11):
 
@@ -40,11 +54,12 @@ if __name__ == '__main__':
           searchType='image',
           imgColorType='color',
           imgType='photo',
-          start=(page-1)*NUM+1
+          start=(page-1)*PER_PAGE+1
         ).execute()
 
         links += [i['link'] for i in res['items']]
 
+    # Place the image directory at the current path.
     if not path.exists(directory):
         mkdir(directory)
     chdir(directory)
